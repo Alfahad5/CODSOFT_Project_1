14ca470ac0ad9496cddc3587ac117e21505d9046
Alfahad5 <144560282+Alfahad5@users.noreply.github.com>
2023-10-10 17:35:16 +0530

    Updated Number_Game.java

--- Number_Game.java
+++ Number_Game.java
@@ -1,4 +1,4 @@
-package Games;
+
 // GUESSING GAME!
 import java.util.Random;
 import java.util.Scanner;
@@ -7,8 +7,8 @@ class Game{
     int n;
 
     Random r = new Random();
-    public int generateNum(){
-       return r.nextInt(1,100);
+    public int generateNum(){// Generates Random number from 1 to 100
+       return r.nextInt(1,101);
     }
     public int takeUserInput() {
         Scanner sc = new Scanner(System.in);
@@ -16,7 +16,7 @@ class Game{
         n=sc.nextInt();
         return n;
     }
-    public boolean isCorrect(int n,int rn) {
+    public boolean isCorrect(int n,int rn) { // Checks the Guessed Number
         if(n==rn) {
             return false;
         }
@@ -27,12 +27,12 @@ class Game{
             System.out.println("Too HIGH!\n\nGuess Again ");
         return true;
     }
-    void noOfGuess(int c) {
+    void noOfGuess(int c) { // Counts the number of Guesses 
         System.out.println("You Guessed "+c+" Times");
     }
 
-}
-public class Guess {
+}//End of Class Game
+public class Number_Game{
     public static void main(String[] args) {
         Game g = new Game();
         boolean a= true;
@@ -50,15 +50,15 @@ public class Guess {
 
 
         }
-        if(count<11) {
-            System.out.println("You WON! OMEDETO!");
+        if(count<11) {//Wining Condition
+            System.out.println("You WON! Congratulations!");
             g.noOfGuess(count);
 
         }
         else {
-            System.out.println("SED! You Loose. \nBetter Luck Next Time! ");
+            System.out.println("Nice Try!. \nBetter Luck Next Time! ");
 
         }
 
-    }
-}
+    }// End of main
+}// End of Guess
